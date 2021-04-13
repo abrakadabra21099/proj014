4a5aeaaf27cb4cc0c5994124ac5586fbc82571cc
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-13 18:19:47 +0400

    Learn 44. Done.

--- js/script.js
+++ js/script.js
@@ -93,4 +93,41 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     setClock('.timer', deadLine);
+
+    // Modal
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtns = document.querySelectorAll('[data-close]');
+
+    modalTrigger.forEach(value => {
+        value.addEventListener('click', () => {
+            // modal.classList.add('show');
+            // modal.classList.remove('hide');
+            modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function hideModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtns.forEach(value => {
+        value.addEventListener('click', hideModal);
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            hideModal();
+            // modal.classList.toggle('show');
+            // document.body.style.overflow = '';
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            hideModal();
+        }
+    });
 });
